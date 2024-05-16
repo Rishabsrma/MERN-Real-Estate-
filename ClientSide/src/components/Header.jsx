7942932976c1a8bad3998fd43057d7184c4bc10f
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2024-05-16 18:04:14 +0545

    minor classname change

--- ClientSide/src/components/Header.jsx
+++ ClientSide/src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
           <input
             type="text"
             placeholder="search..."
-            className="bg-transaparent focus:outline-none w-24 sm:w-64"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
           <FaSearch className="text-slate-600" />
         </form>

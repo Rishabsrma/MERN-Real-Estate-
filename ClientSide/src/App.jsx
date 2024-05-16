c06eb552df796a7cdf13fcfa9b705651247b0b54
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2024-05-16 17:29:18 +0545

    created pages and routes

--- ClientSide/src/App.jsx
+++ ClientSide/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
-const App = () => {
-  return <h1 className="text-red-500">Hi</h1>;
-};
-
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}

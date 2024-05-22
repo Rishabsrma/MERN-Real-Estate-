bfdaad6c6c8314246faddc4cd8dcd2e507f24c7c
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2024-05-22 22:22:19 +0545

    Created signup api route and hashing for password

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);

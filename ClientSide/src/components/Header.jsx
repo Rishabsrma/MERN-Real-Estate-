c8b786e9310717249bd26e60c6249a169848d18f
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2024-06-13 20:08:22 +0545

    Completed signup page with api functionality

--- ClientSide/src/components/Header.jsx
+++ ClientSide/src/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
-            placeholder="search..."
+            placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
           <FaSearch className="text-slate-600" />

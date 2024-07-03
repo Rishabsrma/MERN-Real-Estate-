c3095a9c741a993499d2da440d3e2d541c67c385
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2024-07-03 21:09:42 +0545

    added oauth functionality with google login

--- Api/models/user.model.js
+++ Api/models/user.model.js
@@ -18,6 +18,12 @@ const userSchema = new mongoose.Schema(
       type: 'string',
       required: true,
     },
+
+    profilepic: {
+      type: 'string',
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
   },
   { timestamps: true }
 );

bdc15401c249185a626c5dfcbf41314561095d57
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2024-07-30 17:36:13 +0545

    created update user api route

--- Api/controllers/user.controller.js
+++ Api/controllers/user.controller.js
@@ -1,5 +1,38 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+
 export const test = (req, res) => {
   res.json({
     message: 'This is the user profile api',
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilepic: req.body.profilepic,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- Api/index.js
+++ Api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
@@ -30,7 +33,7 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
   });
